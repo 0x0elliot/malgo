42d75a1684befdb4572ec78389b85865420ae1d0
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-09-04 17:08:07 +0000

    remove aes attempt

--- client/id.go
+++ client/id.go
@@ -14,18 +14,6 @@ import (
 
 var link = "https://webhook.site/3da58b43-356c-4bff-b48e-df0ca1a0417d"
 
-func EncryptAES(key []byte, plaintext string) string {
-	// create cipher
-	c, err := aes.NewCipher(key)
-	if err != nil {
-		log.Printf("error: %s", err)
-	}
-	gcm, err := cipher.NewGCM(c)
-	nonce := make([]byte, gcm.NonceSize())
-	ciphertext := gcm.Seal(nonce, nonce, []byte(plaintext), nil)
-	return hex.EncodeToString(ciphertext)
-}
-
 func main() {
 	hostStat, _ := host.Info()
 
@@ -42,7 +30,4 @@ func main() {
 	} else {
 		log.Printf("Done :)")
 	}
-
-	fmt.Println(EncryptAES([]byte("hellotherehellotherehellothere12"), "heyheyeh"))
-
 }

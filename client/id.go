fe0b90524019e5227685d07383d0a56dd866ce2a
RoguedBear <39442192+RoguedBear@users.noreply.github.com>
2022-09-07 22:30:38 +0530

    move to using openssl aes encryption... ...instead of making one in-house :)

--- client/id.go
+++ client/id.go
@@ -1,28 +1,41 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"encoding/hex"
-	"fmt"
 	"log"
 	"net/http"
 	"net/url"
+	"os/exec"
+	"strings"
 
 	"github.com/shirou/gopsutil/host"
 )
 
-var link = "https://webhook.site/3da58b43-356c-4bff-b48e-df0ca1a0417d"
+var link = "https://webhook.site/6fa01dd1-d7af-4e84-b221-1e72c55ca5fe"
+var plaintext_key = "very-long-plaintext-key:)"
 
 func main() {
 	hostStat, _ := host.Info()
 
-	fmt.Printf("%+v\n", hostStat)
 	data := url.Values{
 		"host": {hostStat.HostID},
 	}
 
-	_, err := http.PostForm(link, data)
+	cmd := exec.Command("/usr/bin/openssl", "aes-128-cbc", "-e", "-a", "-pbkdf2", "-k", plaintext_key)
+	cmd.Stdin = strings.NewReader(data.Encode())
+	out, err := cmd.Output()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	req, err := http.NewRequest("POST", link, strings.NewReader(string(out)))
+
+	if err != nil {
+		panic(err)
+	}
+
+	client := http.Client{}
+	_, err = client.Do(req)
 
 	if err != nil {
 		log.Printf("Request Failed: %s", err)
@@ -30,4 +43,5 @@ func main() {
 	} else {
 		log.Printf("Done :)")
 	}
+
 }
